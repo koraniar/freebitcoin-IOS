d8fc841d09f3410c345559a1fcf1dd0ee0c6d97a
Koraniar  <esteban.garcia@tulpep.com>
2020-06-16 21:18:55 -0500

    Display view

--- freebitcoin/ViewController.swift
+++ freebitcoin/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myURL = URL(string:"https://freebitco.in/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
-
-
 }
+
 
