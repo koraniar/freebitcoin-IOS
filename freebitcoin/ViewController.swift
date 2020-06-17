84c055bc147045c7f640065ae16947d80804db79
Koraniar  <esteban.garcia@tulpep.com>
2020-06-16 21:32:26 -0500

    Schedule notification or display alert

--- freebitcoin/ViewController.swift
+++ freebitcoin/ViewController.swift
@@ -25,6 +25,42 @@ class ViewController: UIViewController, WKUIDelegate {
         let myURL = URL(string:"https://freebitco.in/")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
+        
+        let buttonFrame = CGRect(x: 50, y: 150, width: 50, height: 50)
+        let button = UIButton(frame: buttonFrame)
+        button.backgroundColor = UIColor.green
+        button.addTarget(self, action: #selector(scheduleNotification(sender:)), for: .touchUpInside)
+        webView.addSubview(button)
+    }
+    
+    @objc func scheduleNotification(sender: UIButton) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) { (allowed, _) in
+            if allowed {
+                let content = UNMutableNotificationContent()
+                content.title = "Notification"
+                content.body = "Hello From Kavsoft !!!"
+
+                // this time interval represents the delay time of notification
+                // ie., the notification will be delivered after the delay.....
+
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                let request = UNNotificationRequest(identifier: "noti", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+
+                return
+            } else {
+                let alert = UIAlertController(title: "Error", message: "The notifications are disabled, please enable it in settings.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")
+                }))
+                
+                DispatchQueue.main.async {
+                  self.present(alert, animated: true, completion: nil)
+                }
+
+            }
+        }
     }
 }
 
