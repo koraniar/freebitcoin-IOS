fae39fc83af80ffa087e8ae91f669afc53aed199
Koraniar  <esteban.garcia@tulpep.com>
2020-06-17 21:06:44 -0500

    Display countdown

--- freebitcoin/ViewController.swift
+++ freebitcoin/ViewController.swift
@@ -36,31 +36,54 @@ class ViewController: UIViewController, WKUIDelegate {
     @objc func scheduleNotification(sender: UIButton) {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) { (allowed, _) in
             if allowed {
-                let content = UNMutableNotificationContent()
-                content.title = "Notification"
-                content.body = "Hello From Kavsoft !!!"
-
-                // this time interval represents the delay time of notification
-                // ie., the notification will be delivered after the delay.....
-
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                let request = UNNotificationRequest(identifier: "noti", content: content, trigger: trigger)
-                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+                DispatchQueue.main.async {
+                  let datePicker: UIDatePicker = UIDatePicker()
+                  datePicker.frame = CGRect(x: 0, y: 50, width: self.webView.frame.width, height: 200)
+                  datePicker.datePickerMode = .countDownTimer
+                  datePicker.countDownDuration = 60 * 60
+                  datePicker.backgroundColor = UIColor.white
+                  datePicker.addTarget(self, action: #selector(self.datePickerValueChanged(_:)), for: .valueChanged)
+                  
+                  self.webView.addSubview(datePicker)
+                }
+                
+                
+//                // Schedule Notification
+//                let content = UNMutableNotificationContent()
+//                content.title = "Notification"
+//                content.body = "Hello From Kavsoft !!!"
+//
+//                // this time interval represents the delay time of notification
+//                // ie., the notification will be delivered after the delay.....
+//
+//                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//
+//                let request = UNNotificationRequest(identifier: "noti", content: content, trigger: trigger)
+//                UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
 
                 return
             } else {
                 let alert = UIAlertController(title: "Error", message: "The notifications are disabled, please enable it in settings.", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-                    NSLog("The \"OK\" alert occured.")
+//                    TODO
+//                    Open Settings
                 }))
                 
                 DispatchQueue.main.async {
                   self.present(alert, animated: true, completion: nil)
                 }
-
             }
         }
+    }
+    
+    // Not called the first time
+    @objc func datePickerValueChanged(_ sender: UIDatePicker){
+        print("Selected value \(sender.countDownDuration)")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
 
